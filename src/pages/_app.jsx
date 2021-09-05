89a7e46b0e664b780920f1a615e698d2731b161c
kosuke-17 <tamutamu17@icloud.com>
2021-09-05 16:09:27 +0900

    _appにレイアウトを表示

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,31 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
+import Head from 'next/head'
+import BackBoard from '../components/BackBoard'
+import { Layout } from 'antd';
+import 'antd/dist/antd.css';
+import SideNav from '../components/SideNav';
+
+const { Sider, Content } = Layout;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <Head>
+      <title>CC株式会社</title>
+    </Head>
+    <Layout>
+      <Sider>
+        <SideNav />
+      </Sider>
+      <Content className="overflow-x-auto">
+        <BackBoard>
+          <Component {...pageProps} />
+        </BackBoard>
+      </Content>
+    </Layout>
+  </>
+  )
 }
 
 export default MyApp
