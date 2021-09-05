9f15c8f8a90e046944428e1fd9a19df5f130334f
kosuke-17 <tamutamu17@icloud.com>
2021-09-05 15:08:54 +0900

    Linkでコンポネートの切り替え処理

--- src/components/SideNav/index.jsx
+++ src/components/SideNav/index.jsx
@@ -1,10 +1,11 @@
 import { Menu } from "antd"
+import Link from 'next/link'
 
 const SideNav = () => {
   return (
     <>
     <div className="p-3 ">
-        <a href="#" className="pl-3 text-2xl text-white">CC株式会社</a>
+        <a href="#" className="pl-3 text-lg text-white">CCアプリについて</a>
     </div>
     <Menu
       mode="inline"
@@ -12,8 +13,21 @@ const SideNav = () => {
       defaultOpenKeys={['sub1']}
       style={{ height: '100vh', borderRight: 0, paddingTop: 20, backgroundColor: "#DDDDDD" }}
     >
-      <Menu.Item key="1">FAQ~よくあるご質問~</Menu.Item>
-      <Menu.Item key="2">WEBチャットbotへ</Menu.Item>
+      <Menu.Item key="1">
+        <Link href="/">
+          FAQ~よくあるご質問~
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="2">
+        <Link href="/moveToChatBot">
+          WEBチャットbotへ
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="3">
+        <Link href="/contactForm">
+          お問い合わせ
+        </Link>
+        </Menu.Item>
     </Menu>
   </>
   )
