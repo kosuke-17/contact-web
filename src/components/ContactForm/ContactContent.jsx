842d0d1fb6fd14d72ed65751638209909ca09379
kosuke-17 <tamutamu17@icloud.com>
2021-09-08 13:00:03 +0900

    投稿内容の表示

--- src/components/ContactForm/ContactContent.jsx
+++ src/components/ContactForm/ContactContent.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import { Card } from 'antd';
 
-const contactContent = ({inputContents, id, name, email, introduction}) => {
-  // 代入すれば切ると思ったができなかった
-  const contents = inputContents
-  console.log(inputContents)
-  // オブジェクトを表示させたい(まだできてない)
+const contactContent = ({inputContents}) => {
   return (
     <Card
       style={{ width: '780px', marginTop: 20 }}
       title="Slackに送信したお問い合わせの内容"
     >
-      <p>
-        名前：{contents.name}
-      </p>
-      <p>
-        アドレス：{contents.email}
-      </p>
-      <p>
-        内容：{contents.introduction}
-      </p>
+      {inputContents.map((inputContent) => {
+        return (
+          <ul>
+            <li>
+              名前：{inputContent.name}
+            </li>
+            <li>
+              アドレス：{inputContent.email}
+            </li>
+            <li>
+              内容：{inputContent.introduction}
+            </li>
+          </ul>
+        )
+      })}
     </Card>
   )
 }
